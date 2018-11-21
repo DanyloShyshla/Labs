9bd15bccf4eaa92e9f11c9818b6824aaa62b4efa
DanyloShyshla <dshyshla@gmail.com>
2018-11-21 19:47:19 +0200

    Lab-3
    Variable names updated.

--- Lab-3.c
+++ Lab-3.c
@@ -1,55 +1,60 @@
 #include <stdio.h>
 #include <math.h>
-#define n 5
-void sort(int array[n][n]);
-void avrg(int array[n][n]);
-int main() {
-     int array[n][n] ={
-             {-12, 7, 23, 13, 4},
-             {-5, 23, 45, 67, -2},
-             {0, 15, 4, 9, -14},
-             {1, -4, 6, -2, 0},
-             {3, 33, -1, 0, -78}
 
-     };
-    sort(array);
-    avrg(array);
+#define matrix_Size 5
+
+void matrix_Bubble_Sort(int array[matrix_Size][matrix_Size]);
+
+void avrg_Arithmetical_Number(int array[matrix_Size][matrix_Size]);
+
+int main() {
+    int array[matrix_Size][matrix_Size] = {
+            {-12, 7,  23, 13, 4},
+            {-5,  23, 45, 67, -2},
+            {0,   15, 4,  9,  -14},
+            {1,   -4, 6,  -2, 0},
+            {3,   33, -1, 0,  -78}
+
+    };
+    matrix_Bubble_Sort(array);
+    avrg_Arithmetical_Number(array);
     return 0;
 }
-void sort(int array[n][n]){
-    int i, k, j, c;
-    for(i = 0; i < n; i++)
-    {
-        for(k = n-1; k>=0; k--) {
-            for (j = 0; j < k; j++) {
-                if (array[i][j] < array[i][j + 1]) {
-                    c = array[i][j];
-                    array[i][j] = array[i][j + 1];
-                    array[i][j + 1] = c;
+
+void matrix_Bubble_Sort(int array[matrix_Size][matrix_Size]) {
+    int row, temporary_Index, column, element;
+    for (row = 0; row < matrix_Size; row++) {
+        for (temporary_Index = matrix_Size - 1; temporary_Index >= 0; temporary_Index--) {
+            for (column = 0; column < temporary_Index; column++) {
+                if (array[row][column] < array[row][column + 1]) {
+                    element = array[row][column];
+                    array[row][column] = array[row][column + 1];
+                    array[row][column + 1] = element;
                 }
             }
         }
     }
-    for(i=0; i<n; i++){
-        for (j = 0; j < n; j++) {
-            printf("%d\t",array[i][j]);
+    for (row = 0; row < matrix_Size; row++) {
+        for (column = 0; column < matrix_Size; column++) {
+            printf("%d\t", array[row][column]);
         }
         printf("\n");
 
     }
 }
-void avrg(int array[n][n]) {
-    int i,j,s;
-    float k = 1;
-    for (i = 0; i < n; i++) {
-        s = 0;
-        for (j = 0; j < n; j++) {
-            s += array[i][j];
-        }
-        float output = (float) s/n;
-        k = output*k;
 
-        printf("f() = %f\n",output);
-        printf("*() = %f\n",k);
+void avrg_Arithmetical_Number(int array[matrix_Size][matrix_Size]) {
+    int row, column, next_Column;
+    float average_Arithmetical_Number = 1;
+    for (row = 0; row < matrix_Size; row++) {
+        next_Column = 0;
+        for (column = 0; column < matrix_Size; column++) {
+            next_Column += array[row][column];
+        }
+        float output = (float) next_Column / matrix_Size;
+        average_Arithmetical_Number = output * average_Arithmetical_Number;
+
+        printf("f() = %f\n", output);
+        printf("*() = %f\n", average_Arithmetical_Number);
     }
 }
