3f918c8c8b3c1b2d04f08f408686fad6d88a1813
DanyloShyshla <dshyshla@gmail.com>
2018-11-21 20:23:07 +0200

    Lab-3
    Variable names updated

--- Lab-3.c
+++ Lab-3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-#define matrix_Size 5
+#define MATRIX_SIZE 5
 
-void matrix_Bubble_Sort(int array[matrix_Size][matrix_Size]);
+void matrix_bubble_sort(int array[MATRIX_SIZE][MATRIX_SIZE]);
 
-void avrg_Arithmetical_Number(int array[matrix_Size][matrix_Size]);
+void avrg_arithmetical_number(int array[MATRIX_SIZE][MATRIX_SIZE]);
 
 int main() {
-    int array[matrix_Size][matrix_Size] = {
+    int array[MATRIX_SIZE][MATRIX_SIZE] = {
             {-12, 7,  23, 13, 4},
             {-5,  23, 45, 67, -2},
             {0,   15, 4,  9,  -14},
@@ -16,15 +16,15 @@ int main() {
             {3,   33, -1, 0,  -78}
 
     };
-    matrix_Bubble_Sort(array);
-    avrg_Arithmetical_Number(array);
+    matrix_bubble_sort(array);
+    avrg_arithmetical_number(array);
     return 0;
 }
 
-void matrix_Bubble_Sort(int array[matrix_Size][matrix_Size]) {
+void matrix_bubble_sort(int array[MATRIX_SIZE][MATRIX_SIZE]) {
     int row, temporary_Index, column, element;
-    for (row = 0; row < matrix_Size; row++) {
-        for (temporary_Index = matrix_Size - 1; temporary_Index >= 0; temporary_Index--) {
+    for (row = 0; row < MATRIX_SIZE; row++) {
+        for (temporary_Index = MATRIX_SIZE - 1; temporary_Index >= 0; temporary_Index--) {
             for (column = 0; column < temporary_Index; column++) {
                 if (array[row][column] < array[row][column + 1]) {
                     element = array[row][column];
@@ -34,8 +34,8 @@ void matrix_Bubble_Sort(int array[matrix_Size][matrix_Size]) {
             }
         }
     }
-    for (row = 0; row < matrix_Size; row++) {
-        for (column = 0; column < matrix_Size; column++) {
+    for (row = 0; row < MATRIX_SIZE; row++) {
+        for (column = 0; column < MATRIX_SIZE; column++) {
             printf("%d\t", array[row][column]);
         }
         printf("\n");
@@ -43,18 +43,18 @@ void matrix_Bubble_Sort(int array[matrix_Size][matrix_Size]) {
     }
 }
 
-void avrg_Arithmetical_Number(int array[matrix_Size][matrix_Size]) {
+void avrg_arithmetical_number(int array[MATRIX_SIZE][MATRIX_SIZE]) {
     int row, column, next_Column;
-    float average_Arithmetical_Number = 1;
-    for (row = 0; row < matrix_Size; row++) {
+    float avrg_arithmetical_number = 1;
+    for (row = 0; row < MATRIX_SIZE; row++) {
         next_Column = 0;
-        for (column = 0; column < matrix_Size; column++) {
+        for (column = 0; column < MATRIX_SIZE; column++) {
             next_Column += array[row][column];
         }
-        float output = (float) next_Column / matrix_Size;
-        average_Arithmetical_Number = output * average_Arithmetical_Number;
+        float output = (float) next_Column / MATRIX_SIZE;
+        avrg_arithmetical_number = output * avrg_arithmetical_number;
 
         printf("f() = %f\n", output);
-        printf("*() = %f\n", average_Arithmetical_Number);
+        printf("*() = %f\n", avrg_arithmetical_number);
     }
 }
